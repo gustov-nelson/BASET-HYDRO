3550b29fc8d9ce53b25dd10b3da2008539ebd76e
gustov-nelson <134996654+gustov-nelson@users.noreply.github.com>
2023-05-30 02:27:59 -0700

    Added items

--- bom.py
+++ bom.py
@@ -1,13 +1,17 @@
- products = [
+products = [
        # ["1in. x 10ft. PVC pipe", 'https://www.homedepot.com/p/Charlotte-Pipe-1-in-x-10-ft-Plastic-Plain-End-Pipe-PVC200100600/100348483', 14],
         ["#8 x 1 in. Wood Screws (50 per Pack)", 'https://www.homedepot.com/p/Everbilt-8-x-1-in-Coarse-Zinc-Plated-Phillips-Bugle-Head-Wood-Screws-50-per-Pack-802672/204283331', 1],
         ['106 Qt. Latching Storage Box', 'https://www.homedepot.com/p/Sterilite-106-Qt-Latching-Storage-Box-14998004/206721484', 1],
         ['Raspberry Pi 4', 'https://www.amazon.com/Raspberry-Pi-Computer-Suitable-Workstation/dp/B0899VXM8F/?_encoding=UTF8&content-id=amzn1.sym.bc5f3394-3b4c-4031-8ac0-18107ac75816&ref_=pd_gw_ci_mcx_mr_hp_atf_m', 1],
         ['Ratchet Pulley', 'https://www.amazon.com/Pairs-inch-Adjustable-Heavy-Hanger/dp/B07XKLLVL7/ref=sr_1_5?keywords=ratchet+pulley&sr=8-5', 1],
-        ['Panda','https://www.vivosun.com/vivosun-5-gallon-grow-bags-5-pack-black-thickened-nonwoven-fabric-pots-with-handles-p68320123310965170-v58820960379602944?_lf=buyWith',1],
-        ['1kg Black PLA filament','https://www.amazon.com/OVERTURE-Filament-Consumables-Dimensional-Accuracy/dp/B07PGY2JP1/ref=sr_1_1_sspa?keywords=PLA%2Bfilament&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQUFQTU1YSk5MNzZJJmVuY3J5cHRlZElkPUEwNzYzODU1M1JINjVRWFREVDQyMCZlbmNyeXB0ZWRBZElkPUEwMTE2NjI0MVpOR0o1RjFWMThLNyZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU&th=1',1]
-    ]
+        ['1ft x 100ft Panda film','https://www.amazon.com/VIVOSUN-Poly-Black-White-Panda/dp/B018VI72S0/ref=sr_1_1?keywords=VIVOSUN%2BBlack%2Band%2BWhite%2BPanda%2BFilm%2C%2B10%E2%80%99%2Bx%2B50%E2%80%99%2B5.5%2BMil%2BPoly%2BFilm&s=lawn-garden&sr=1-1&ufe=app_do%3Aamzn1.fos.18630bbb-fcbb-42f8-9767-857e17e03685&th=1',1],
+        ['1kg Black PLA filament','https://www.amazon.com/OVERTURE-Filament-Consumables-Dimensional-Accuracy/dp/B07PGY2JP1/ref=sr_1_1_sspa?keywords=PLA%2Bfilament&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQUFQTU1YSk5MNzZJJmVuY3J5cHRlZElkPUEwNzYzODU1M1JINjVRWFREVDQyMCZlbmNyeXB0ZWRBZElkPUEwMTE2NjI0MVpOR0o1RjFWMThLNyZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU&th=1',1],
+        ['16GB microSD card','https://www.amazon.com/Sandisk-Ultra-Micro-UHS-I-Adapter/dp/B073K14CVB/ref=sr_1_3?keywords=micro+sd+card&nav_sdd=aps&refinements=p_n_feature_two_browse-bin%3A6518303011&rnid=6518301011&s=pc&sr=1-3',1],
+        ['Submersible Water Pump','https://www.amazon.com/gp/product/B086QJQK8N/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&th=1',1],
+        ['Drip Irrigation Kit','https://www.amazon.com/gp/product/B0B217QKHH/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1',1],
+        ]
 
-    append_list = [
-        ["1in. x 10ft. PVC pipe", 'https://www.homedepot.com/p/Charlotte-Pipe-1-in-x-10-ft-Plastic-Plain-End-Pipe-PVC200100600/100348483', 14, float(4.41)],
-    ]
+append_list = [
+        ["1in. x 10ft. PVC pipe", 'https://www.homedepot.com/p/Charlotte-Pipe-1-in-x-10-ft-Plastic-Plain-End-Pipe-PVC200100600/100348483', 14, float(3.53)],
+        ["1.5in. x 10ft. ABS pipe",'https://www.homedepot.com/p/VPC-1-5-in-x-10-ft-ABS-Cell-Core-Pipe-29-1510HD/309282454#overlay',1,float(14.46)]
+        ]
